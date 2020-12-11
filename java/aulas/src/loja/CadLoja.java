da5c2baf001da111cd64b1e7d9820bf2d374b83d
beymarjhoel <beymarjhlima@gmail.com>
2020-12-11 09:27:35 -0300

    Atualizando da minha loja Dragon Platinum

--- java/aulas/src/loja/CadLoja.java
+++ java/aulas/src/loja/CadLoja.java
@@ -5,119 +5,118 @@ import java.util.Scanner;
 public class CadLoja {
 
 	public static void main(String[] args) {
-
-		Scanner scan = new Scanner(System.in);
 		
+		Scanner leia = new Scanner(System.in);
 		String codigos[] = new String[10];
-		String produtos[] = new String[10];
-		double precoUnitarios[] = new double[10];
+		String produtos[] = {"JOGO DE XADREZ","JOGO DE DAMAS","JOGO DE CARTAS UNO","QUEBRA-CEBEÇA","JOGO MONOPOLY","JOGO DETETIVE","JOGO DE DOMINÓ","JOGO DE ENIGMAS","JOGO DE BATALHA NAVAL","JOGO DE BARALHO DE CARTAS"};
+		double precoUnitarios[] = {10,10,10,10,10,10,10,10,10,10};
 		int estoque[] = new int[10];
-		
-		char generoCliente;
-		char opcaoMenu, opcao;
-		String nomeCliente;
-		
-		do { 
-			inseriLinha(80, "▬");
-			
-			System.out.println("\t\t\t        DRAGON PLATINUM");
-			System.out.println("\t\t\t    [Juntos jogamos melhor]");
-			inseriLinha(80, "▬");
-			pula();
-	
-			System.out.println("[1] - Comprar Produtos");
-			System.out.println("[2] - Gerenciar Estoques");
-			System.out.println("[3] - Sair");
-			pula();
-			System.out.print("Digite a opção: ");
-			opcaoMenu = scan.next().charAt(0);
-			pula();
-			
-			switch(opcaoMenu) {
-				case '1':
-					inseriLinha(80, "▬");
-					System.out.println("Dragon Platinum");
-					inseriLinha(80, "▬");
-					System.out.print("Nome do cliente: ");
-					nomeCliente = scan.next().toUpperCase();
-					System.out.print("Gênero do cliente [M] Masculino [F] Feminino [O] Outro: ");
-					generoCliente = scan.next().toUpperCase().charAt(0);
-				
-					if(voltaGenero(generoCliente) == "Masculino") {
-						pula();
-						System.out.printf("Seja bem vindo a loja %s!",nomeCliente);
-						pula();
-						System.out.print("Deseja continuar ? [S/N]: ");
-						opcao = scan.next().toUpperCase().charAt(0);
-						
-						
-					} else if (voltaGenero(generoCliente) == "Feminino") {
-						pula();
-						System.out.printf("Seja bem vinda a loja %s!",nomeCliente);
-						do {
-							System.out.print("Deseja continuar ? [S/N]: ");
-							opcao = scan.next().toUpperCase().charAt(0);
-						} while(opcao == 'S');
-					} else if (voltaGenero(generoCliente) == "Outros") {
-						pula();
-						System.out.printf("Seja bem vindx a loja %s!",nomeCliente);
-						do {
-							System.out.print("Deseja continuar ? [S/N]: ");
-							opcao = scan.next().toUpperCase().charAt(0);
-						} while(opcao == 'S');
-					}
+		int carrinho[];
+		String sacola;
+		int aux = 0;
 					
-				break;
+		criarLinha(80, "▬");
+		System.out.println("\t\t\t\tDRAGON PLATINUM");
+		System.out.println("\t\t\t   [Juntos jogamos melhor!]");
+
+		criarLinha(80, "▬");
+		System.out.println();
+
+		System.out.println("[1] - COMPRAR PRODUTOS");
+		System.out.println("[2] - GERENCIAR ESTOQUE");
+		System.out.println("[3] - SAIR");
+		System.out.println();
+		System.out.print("DIGITE A OPÇÃO: ");
+		char opcaoMenu = leia.next().charAt(0);
+		
+		switch (opcaoMenu) {
+			case '1': 
+				criarLinha(80, "▬");
+				System.out.println("\t\t\t\tDRAGON PLATINUM");
+				criarLinha(80, "▬");
 				
-				case '2':
-					pula();
-					System.out.println("WIP");	//Work in progress
-				break;
+				System.out.print("Por favor informe o seu nome: ");
+				String nomeCliente = leia.next().toUpperCase();
+				System.out.print("\nVocê de define como [M] - Masculino, [F] - Feminino ou [O] - Outro : ");
+				char generoCliente = leia.next().toUpperCase().charAt(0);
+				System.out.printf("\nSeja bem-vind%s a loja %s!!!\n",voltaGenero(generoCliente),nomeCliente);
 				
-				case '3':
-					pula();
-					System.out.println("Obrigado por visitar a nossa loja, adoramos sua visita!");
-				break;
-			}
+				criarLinha(80, "▬");
+				System.out.println("CODIGO\t\tR$(UN)\t\tESTOQUE\t\tPRODUTO");
+				criarLinha(80, "▬");
+				
+				for (int x=0; x<10; x++) {
+					
+					estoque[x]=10;
+					codigos[x]=(x<9)?"DGP-00"+(x+1):"DGP-0"+(x+1);
+					System.out.printf("%s\t\t%.2f\t\t  %d\t\t%s\n",codigos[x],precoUnitarios[x],estoque[x],produtos[x]);
+					
+				}
+				criarLinha(80, "▬");
+				System.out.print("Insira o código do produto que deseja comprar: ");
+				sacola = leia.next();
+				
+				for(int x = 0; x < 10; x++) {
+					if(codigos[x].equals(sacola)) {
+						aux = x;
+					}
+				}
+				
+				System.out.println("\nProduto escolhido: " + produtos[aux]);
+				
+			break;
+				
 			
-		} while(opcaoMenu != '3');
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+			case '2':
+			
+				System.out.println("WIP");  // work in progress
+			break;
+			
+			case '3':
+			
+				System.out.println("Volte sempre, adoramos sua visita!!!");
+			break;
+			
+				
+		}
+			
 	}
+	
+	
+	
+	
+
+	
+	public static void criarLinha(int tamanho, String simbolo) {
+			for (int i = 0; i < tamanho; i++) {
+				System.out.print(simbolo);
+			}
+			System.out.println();
+		}
 	
 	public static String voltaGenero(char generoCliente) {
-
+		
 		String tipo;
-		if (generoCliente == 'M') {
-			tipo = "Masculino";
-		} else if (generoCliente == 'F') {
-			tipo = "Feminino";
-		} else if (generoCliente == 'O') {
-			tipo = "Outros";
-		} else {
-			tipo = "N/D"; // sem dados
+		if (generoCliente == 'M')
+		{
+			tipo = "o";
 		}
-
+		else if (generoCliente == 'F')
+		{
+			tipo = "a";
+		}
+		else if (generoCliente == 'O')
+		{
+			tipo = "x";
+		}
+		else
+		{
+			tipo = "*";
+		}
+			
+		
 		return tipo;
+	
 	}
-
-	public static void pula() {
-		System.out.print("\n");
-	}
-
-	public static void inseriLinha(int tamanho, String simbolo) {
-		for (int i = 0; i < tamanho; i++) {
-			System.out.print(simbolo);
-		}
-		System.out.println();
-	}
+	
 }
-
